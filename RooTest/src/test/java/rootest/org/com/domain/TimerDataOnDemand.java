907625fa4899ec04fcc1d9291cc817cbd4f82c34
kazunari3 <canser@bu.iij4u.or.jp>
2014-12-13 20:44:58 +0900

    mvc setup
    roo> jpa setup --provider HIBERNATE --database HYPERSONIC_IN_MEMORY
    roo> entity jpa --class ~.domain.Timer --testAutomatically
    roo> field string --fieldName message --notNull
    roo> hint web mvc
    roo> web mvc setup
    roo> web mvc all --package ~.web
    roo> selenium test --controller ~.web.TimerController

---
+++ RooTest/src/test/java/rootest/org/com/domain/TimerDataOnDemand.java
@@ -0,0 +1,6 @@
+package rootest.org.com.domain;
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = Timer.class)
+public class TimerDataOnDemand {
+}
---
+++ RooTest/src/test/java/rootest/org/com/domain/TimerIntegrationTest.java
@@ -0,0 +1,11 @@
+package rootest.org.com.domain;
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = Timer.class)
+public class TimerIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
